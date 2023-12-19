a0f3774c50995f386c374f6fb2eb322af12b1731
Sarah Lesher <seal21295@outlook.com>
2023-12-18 19:33:37 -0500

    update final project

--- Module 8/Project/list/public/element.js
+++ Module 8/Project/list/public/element.js
@@ -1,37 +1,60 @@
 const Element = {
-  data() {
-    return {
-      input: false, // display element text by default
-    };
-  },
-  template: `
-  <li> 
-  <span v-if="!input"> {{element.text}} </span>
-  <input v-else type="text" :value="element.text" 
-   @blur="modify($event)" 
-                ref="refInput" />
-      <button @click="remove()"> Remove </button> 
-      <button @click="input=true"> Modify </button>
-  </li>
-  `,
-  props: ["element"],
-  methods: {
-    remove() {
-      // process the click on the Remove button
-      this.$emit("remove", { id: this.element._id });
+    data() {
+        return {
+            inputTitle: false,
+            inputTask: false,
+        };
     },
-    modify(event) {
-      var value = event.target.value; // value entered
-      // in the field
-      this.input = false; // delete input field
-      this.$emit("modify", { id: this.element._id, value: value });
+
+    template: `
+        <li>
+            <button @click="remove()"> Remove </button>
+   
+            <span v-if="!inputTitle"> {{element.text}} </span>
+            <input v-else type="text" :value="element.text" @blur="modify($event)" ref="refInputTitle"/>
+            <button @click="inputTitle=true">Modify Title </button>
+  
+            <span v-if="!inputTask"> {{element.task}}</span>
+            <input v-else type="text" :value="element.task" @blur="task($event)" ref="refInputTask"/>
+            <button @click="inputTask=true">Modify Task </button>
+  
+            <label> Assigned Date </label>
+            <input type="date" :value="element.dateA" @blur="dateA($event)" />
+        </li>
+    `,
+
+    props: ["element"],
+
+    methods: {
+        remove() {
+            this.$emit("remove", { id: this.element._id });
+        },
+
+        modify(event) {
+            var value = event.target.value;
+            this.inputTitle = false;
+            this.$emit("modify", { id: this.element._id, value: value });
+        },
+
+        task(event) {
+            var value = event.target.value;
+            this.inputTask = false;
+            this.$emit("task", { id: this.element._id, value: value });
+        },
+
+        dateA(event) {
+            var value = event.target.value;
+            this.$emit("dateA", { id: this.element._id, value: value });
+        },
     },
-  },
-  emits: ["remove", "modify"],
-  updated() {
-    // check that the ref="refInput" attribute exists, and
-    // if so, give focus to the input field
-    if (this.$refs.refInput) this.$refs.refInput.focus();
-  },
+
+    emits: ["remove", "modify", "task", "dateA"],
+
+    updated() {
+        if (this.$refs.refInputTitle) this.$refs.refInputTitle.focus();
+        if (this.$refs.refInputTask) this.$refs.refInputTask.focus();
+        if (this.$refs.refInputDateA) this.$refs.refInputDateA.focus();
+    },
 };
+
 export default Element;
--- Module 8/Project/list/public/global-app.js
+++ Module 8/Project/list/public/global-app.js
@@ -1,65 +1,128 @@
 import Element from "./element.js";
 const GlobalApp = {
-  data() {
-    return {
-      elements: [], // array of object { text, _id }
-      // (_id = document id in MongoDB)
-    };
-  },
-  components: {
-    Element: Element,
-  },
-  template: `
-  <button @click="add()">Add Element</button>
-  <ul>
-  <Element v-for="(element, index) in elements" 
-  :key="index" :element="element"
-   @remove="remove($event)" @modify="modify($event)"
- />
-  </ul>
-`,
-  methods: {
-    add() {
-      var text = "Element " + (this.elements.length + 1);
-      axios
-        .post("/list", { text: text }) // pass object // {text:text} to // server
-        .then((response) => {
-          this.elements.push({ text: text, _id: response.data.id });
-        });
+    data() {
+        return {
+            elements: [],
+        };
     },
-    remove(params) {
-      var id = params.id;
-      // remove the element with this id from the elements
-      // array
-      this.elements = this.elements.filter(function (element) {
-        if (element._id == id) return false;
-        else return true;
-      });
-      axios.delete("/list", { data: { id: id } }); // the options must be written in the data // property
+
+    components: {
+        Element: Element,
     },
-    modify(params) {
-      var id = params.id;
-      var value = params.value;
-      // modify the text of the element with this id in the
-      // elements array
-      this.elements = this.elements.map(function (element) {
-        if (element._id == id) {
-          element.text = value;
-          return element;
-        } else return element;
-      }); // modify the text of the element having this // identifier
-      axios.put("/list", { text: value, id: id });
+
+    template: `
+        <button @click="add()">Add Task</button>
+        <ul>
+            <Element v-for="(element, index) in elements"
+                :key="index" 
+                :element="element"
+                @remove="remove($event)" 
+                @modify="modify($event)" 
+                @task="task($event)"  
+                @dateA="dateA($event)"  
+            />
+        </ul>
+    `,
+
+    methods: {
+        async add() {
+            var defaultTitle = " Task " + (this.elements.length + 1);
+            var defaultTaskDescription = " ";
+            var defaultDateAssigned = " ";
+            try {
+                const response = await axios.post("/list", {
+                    text: defaultTitle,
+                    task: defaultTaskDescription,
+                    dateA: defaultDateAssigned,
+                });
+                console.log(this.elements);
+                this.elements.push({
+                    _id: response.data.id,
+                    text: defaultTitle,
+                    task: defaultTaskDescription,
+                    dateA: defaultDateAssigned,
+                });
+            } 
+            catch (err) {
+                console.error(err);
+            }
+        },
+
+        remove(params) {
+            var id = params.id;
+            this.elements = this.elements.filter(function (element) {
+                if (element._id == id) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            });
+            axios.delete("/list", { data: { id: id } }); 
+        },
+
+        modify(params) {
+            var id = params.id;
+            var value = params.value;
+            this.elements = this.elements.map(function (element) {
+                if (element._id == id) {
+                    element.text = value;
+                    return element;
+                } 
+                else {
+                    return element;
+                }
+            });
+            axios.put("/list", { text: value, id: id });
+        },
+
+        task(params) {
+            var id = params.id;
+            var value = params.value;
+            this.elements = this.elements.map(function (element) {
+                if (element._id == id) {
+                    element.task = value;
+                    return element;
+                } 
+                else {
+                    return element;
+                }
+            }); 
+            axios.put("/list", { task: value, id: id });
+        },
+
+        dateA(params) {
+            var id = params.id;
+            var value = params.value;
+            this.elements = this.elements.map(function (element) {
+                if (element._id == id) {
+                    element.dateA = value;
+                    return element;
+                } 
+                else {
+                    return element;
+                }
+            });
+            axios.put("/list", { dateA: value, id: id });
+        },
     },
-  },
-  async created() {
-    try {
-      const response = await axios.get("/list");
-      this.elements = response.data.elements.map(function (element) {
-        return { _id: element._id, text: element.text };
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  },
+
+    async created() {
+        try {
+            const response = await axios.get("/list");
+            this.elements = response.data.elements.map(function (element) {
+                return {
+                    _id: element._id,
+                    text: element.text,
+                    task: element.task,
+                    dateA: element.dateA,
+                };
+            });
+        } 
+        catch (err) {
+        console.error(err);
+        }
+    },
 };
+
 export default GlobalApp;
